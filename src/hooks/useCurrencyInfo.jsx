5f7e87756763eda6fad4099c0bec269dc6ed3bbb
Abhay <sharma727485@gmail.com>
2025-02-03 10:46:12 +0530

    added the actions yml file

--- src/hooks/useCurrencyInfo.jsx
+++ src/hooks/useCurrencyInfo.jsx
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
 
 const useCurrencyInfo = (currency) => {
-    const [data, setData] = useState({});
+  const [data, setData] = useState({});
 
-    useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-            .then((res) => res.json())
-            .then((res) => setData(res[currency]));
-    }, [currency]);
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
 
-    return data;
-}
+  return data;
+};
 
 export default useCurrencyInfo;
-
-
-
 
 // let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
